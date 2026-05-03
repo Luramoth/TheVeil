53d8cfecef1711db8971c72ee0482d4e943a4451
LuraMoth <fasreaperearnest@gmail.com>
2026-05-03 03:08:44 -0400

    got it working with Neoforge too

--- common/src/main/java/com/luramoth/theveil/emi/TheVeilModEmiPlugin.java
+++ common/src/main/java/com/luramoth/theveil/emi/TheVeilModEmiPlugin.java
@@ -1,11 +1,13 @@
 package com.luramoth.theveil.emi;
 
 import com.luramoth.theveil.data.CatalystManager;
+import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 
+@EmiEntrypoint
 public class TheVeilModEmiPlugin implements EmiPlugin {
     @Override
     public void register(EmiRegistry emiRegistry) {
