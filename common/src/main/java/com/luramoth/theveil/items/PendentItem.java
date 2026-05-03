e94a1fbb361aadb5254f45c831125eee82d91884
LuraMoth <fasreaperearnest@gmail.com>
2026-05-03 01:37:08 -0400

    add tooltip to the pendant that lists unlocked dimensions using translatable keys

--- common/src/main/java/com/luramoth/theveil/items/PendentItem.java
+++ common/src/main/java/com/luramoth/theveil/items/PendentItem.java
@@ -5,9 +5,11 @@ import com.luramoth.theveil.components.TheVeilModComponents;
 import io.wispforest.accessories.api.Accessory;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 import java.util.List;
 
@@ -20,11 +22,19 @@ public class PendentItem extends Item implements Accessory {
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
         PendantData data = stack.getOrDefault(TheVeilModComponents.PENDENT_DATA.get(), PendantData.DEFAULT);
 
-        tooltipComponents.add(Component.translatable("item.the_veil.pendant.description1").withStyle(ChatFormatting.YELLOW));
-        tooltipComponents.add(Component.translatable("item.the_veil.pendant.description2").withStyle(ChatFormatting.YELLOW));
+        tooltipComponents.add(Component.translatable("item.the_veil.pendant.description1").withStyle(ChatFormatting.GOLD));
+        tooltipComponents.add(Component.translatable("item.the_veil.pendant.description2").withStyle(ChatFormatting.GOLD));
 
         if (!data.unlockedDimensions().isEmpty()) {
             tooltipComponents.add(Component.translatable("item.the_veil.pendant.unlocked_dimensions"));
+
+            for (ResourceKey<Level> dim : data.unlockedDimensions()){
+                String translationKey = "dimension." + dim.location().getNamespace() + "." + dim.location().getPath();
+
+                tooltipComponents.add(Component.literal(" - ")
+                        .append(Component.translatable(translationKey))
+                        .withStyle(ChatFormatting.GRAY));
+            }
         } else {
             tooltipComponents.add(Component.translatable("item.the_veil.pendant.inert").withStyle(ChatFormatting.DARK_GRAY).withStyle(ChatFormatting.ITALIC));
         }
