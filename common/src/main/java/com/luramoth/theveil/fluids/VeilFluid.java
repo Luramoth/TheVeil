eae0d9633c67f5872b5e95a82f613019120b95e2
LuraMoth <fasreaperearnest@gmail.com>
2026-05-04 21:28:54 -0400

    attempt to make Veilfluid again again again again

--- common/src/main/java/com/luramoth/theveil/fluids/VeilFluid.java
+++ common/src/main/java/com/luramoth/theveil/fluids/VeilFluid.java
@@ -7,51 +7,29 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 
-public abstract class VeilFluid extends ArchitecturyFlowingFluid {
-    protected VeilFluid(ArchitecturyFluidAttributes attributes) {
-        super(attributes);
-    }
+public  class VeilFluid {
+    public static class Source extends ArchitecturyFlowingFluid.Source {
 
-    @Override
-    public int getTickDelay(LevelReader level) {
-        return 20;
-    }
-
-    @Override
-    protected void createFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder) {
-        super.createFluidStateDefinition(builder);
-        builder.add(LEVEL);
-    }
-
-    public static class Source extends VeilFluid {
-        public Source(ArchitecturyFluidAttributes attributes){
+        public Source(ArchitecturyFluidAttributes attributes) {
             super(attributes);
         }
 
         @Override
-        public boolean isSource(FluidState state) {
-            return true;
-        }
-
-        @Override
-        public int getAmount(FluidState state) {
-            return 8;
+        protected void createFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder) {
+            super.createFluidStateDefinition(builder);
+            builder.add(LEVEL);
         }
     }
 
-    public static class Flowing extends VeilFluid {
+    public static class Flowing extends ArchitecturyFlowingFluid.Flowing {
         public Flowing(ArchitecturyFluidAttributes attributes) {
             super(attributes);
         }
 
         @Override
-        public boolean isSource(FluidState state) {
-            return false;
-        }
-
-        @Override
-        public int getAmount(FluidState state) {
-            return state.getValue(LEVEL);
+        protected void createFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder) {
+            super.createFluidStateDefinition(builder);
+            builder.add(LEVEL);
         }
     }
 }
